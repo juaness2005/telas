206dbf6a70c9770007ca019c08554360cd61adc7
juaness2005 <juangabriel4362767@gmail.com>
2026-04-14 13:21:19 -0300

    Primeiras telas do projeto de TCC

---
+++ home.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector("#botao-tema")
+const body = document.querySelector("body")
+ 
+botao.addEventListener("click", () => {
+    body.classList.toggle("dark");
+});
+ 
---
+++ login.js
@@ -0,0 +1,9 @@
+const botao = document.querySelector("#botao-tema")
+const body = document.querySelector("body")
+
+botao.addEventListener("click", () => {
+    body.classList.toggle("tema-escuro");
+});
+
+
+
